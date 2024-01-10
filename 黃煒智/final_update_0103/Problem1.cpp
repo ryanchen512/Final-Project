26b098faa96bcaf51ce2460d81d44ed4f6ce1c91
maxhuangw <60605max@gmail.com>
2024-01-11 07:51:10 +0800

    second

--- 黃煒智/final_update_0103/Problem1.cpp
+++ 黃煒智/final_update_0103/Problem1.cpp
@@ -1,7 +1,28 @@
+#include<iostream>
 #include "basicDS.h"
+#include <queue>
+using namespace std;
 
 /* You can add more functions or variables in each class. 
    But you "Shall Not" delete any functions or variables that TAs defined. */
+
+int losesize=0;
+int loseid[100001];
+
+struct cmp{
+	bool operator()(const graphEdge* l, const graphEdge* r){
+		return l->ce > r->ce;
+	}
+};
+
+class Information{
+	public:
+		bool stop=false;
+		int s, t;
+		vector<int> all;
+};
+
+Information inf[100005];
 
 class Problem1 {
 public:
@@ -25,8 +46,72 @@ Problem1::~Problem1() {
 
 void Problem1::insert(int id, int s, Set D, int t, Graph &G, Tree &MTid) {
 	/* Store your output graph and multicast tree into G and MTid */
-	
+	inf[id].s=s;
+	inf[id].t=t;
+	inf[id].all=D.destinationVertices;
+
+	MTid.id=id;
+	MTid.s=s;
+	MTid.ct=0;
+	MTid.V.clear();
+	MTid.E.clear();
+	bool* arr=new bool[100001]();
+
 	/* Write your code here. */
+	priority_queue<graphEdge*, vector<graphEdge*>, cmp> pq;
+
+	int now=s;
+	MTid.V.push_back(now);
+	arr[s]=true;
+	for(auto it:G.E){
+		for(auto &gE:G.E){
+			if(gE.vertex[0]==now&&!arr[gE.vertex[1]]){
+				pq.push(&gE);
+			}
+			if(gE.vertex[1]==now&&!arr[gE.vertex[0]]){
+				pq.push(&gE);
+			}
+		}
+		while(!pq.empty()){
+			//cout<<"{ "<<pq.top()->vertex[0]<<" "<<pq.top()->vertex[1]<<" }";
+			auto gE=pq.top();
+			if(gE->b>=t){
+				if(!(arr[gE->vertex[1]])){
+					gE->b-=t;
+					treeEdge Te;
+					Te.vertex[0]=gE->vertex[0];
+					Te.vertex[1]=gE->vertex[1];
+					arr[gE->vertex[1]]=true;
+					MTid.E.push_back(Te);
+					MTid.ct+=(gE->ce);
+					//cout<<pq.top()->ce<<endl;
+					now=gE->vertex[1];
+					MTid.V.push_back(now);
+					pq.pop();
+					break;
+				}
+				else if(!(arr[gE->vertex[0]])){
+					gE->b-=t;
+					treeEdge Te;
+					Te.vertex[0]=gE->vertex[0];
+					Te.vertex[1]=gE->vertex[1];
+					arr[gE->vertex[0]]=true;
+					MTid.E.push_back(Te);
+					MTid.ct+=(gE->ce);
+					//cout<<pq.top()->ce<<endl;
+					now=gE->vertex[0];
+					MTid.V.push_back(now);
+					pq.pop();
+					break;
+				}
+			}
+			pq.pop();
+		}
+	}
+	if(MTid.V.size()<D.destinationVertices.size()){
+		loseid[losesize++]=MTid.id;
+	}
+	delete [] arr;
 
 	return;
 }
@@ -36,6 +121,109 @@ void Problem1::stop(int id, Graph &G, Forest &MTidForest) {
 	   Note: Please "only" include mutlicast trees that you added nodes in MTidForest. */
 	
 	/* Write your code here. */
+	int newlose=0;
+
+	for(auto it=MTidForest.trees.begin();it!=MTidForest.trees.cend();it++){
+		if(it->id==id){
+			inf[id].stop=true;
+			auto nowtree=&MTidForest.trees[id-1];
+			for(auto ittre=nowtree->E.begin();ittre!=nowtree->E.cend();ittre++){
+				for(auto itgre=G.E.begin();itgre!=G.E.cend();itgre++){
+					if(ittre->vertex[0]==itgre->vertex[0]&&ittre->vertex[1]==itgre->vertex[1]){
+						itgre->b+=inf[id].t;
+						//cout<<ittre->vertex[0]<<" "<<ittre->vertex[1]<<" "<<itgre->b;
+						break;
+					}
+				}
+				//cout<<endl;
+			}
+			nowtree->ct=0;
+			nowtree->E.clear();
+			
+			for(auto nowtreeid=0;nowtreeid<losesize;nowtreeid++){
+				//cout<<inf[nowtreeid].stop<<MTidForest.trees[nowtreeid-1].V.size()<<inf[nowtreeid].all.size()<<endl;
+				if(inf[loseid[nowtreeid]].stop || MTidForest.trees[loseid[nowtreeid]-1].V.size()==inf[loseid[nowtreeid]].all.size()) continue;
+
+				auto MTid=&MTidForest.trees[loseid[nowtreeid]-1];
+				int s=MTid->s;
+				int t=inf[MTid->id].t;
+				auto dis=inf[MTid->id].all;
+				bool* arr=new bool[100001]();
+
+				priority_queue<graphEdge*, vector<graphEdge*>, cmp> pq;
+				int now=s;
+
+				for(auto iter:MTid->V){
+					arr[iter]=true;
+				}
+
+				for(auto iter:MTid->V){
+					for(auto &gE:G.E){
+						if(gE.vertex[0]==iter&&!arr[gE.vertex[1]]){
+							pq.push(&gE);
+						}
+						if(gE.vertex[1]==iter&&!arr[gE.vertex[0]]){
+							pq.push(&gE);
+						}
+					}
+				}
+				
+				for(auto iter:G.E){
+					for(auto &gE:G.E){
+						if(gE.vertex[0]==now&&!arr[gE.vertex[1]]){
+							pq.push(&gE);
+						}
+						if(gE.vertex[1]==now&&!arr[gE.vertex[0]]){
+							pq.push(&gE);
+						}
+					}
+					while(!pq.empty()){
+						//cout<<"{ "<<pq.top()->vertex[0]<<" "<<pq.top()->vertex[1]<<" }";
+						auto gE=pq.top();
+						if(gE->b>=t){
+							//cout<<gE->ce<<" ";
+							if(!(arr[gE->vertex[1]])){
+								gE->b-=t;
+								treeEdge Te;
+								Te.vertex[0]=gE->vertex[0];
+								Te.vertex[1]=gE->vertex[1];
+								arr[gE->vertex[1]]=true;
+								MTid->E.push_back(Te);
+								MTid->ct+=(gE->ce);
+								//cout<<pq.top()->ce<<endl;
+								now=gE->vertex[1];
+								MTid->V.push_back(now);
+								pq.pop();
+								break;
+							}
+							else if(!(arr[gE->vertex[0]])){
+								gE->b-=t;
+								treeEdge Te;
+								Te.vertex[0]=gE->vertex[0];
+								Te.vertex[1]=gE->vertex[1];
+								arr[gE->vertex[0]]=true;
+								MTid->E.push_back(Te);
+								MTid->ct+=(gE->ce);
+								//cout<<pq.top()->ce<<endl;
+								now=gE->vertex[0];
+								MTid->V.push_back(now);
+								pq.pop();
+								break;
+							}
+						}
+						pq.pop();
+					}
+				}
+				
+				if(MTid->V.size()<dis.size()){
+					loseid[newlose++]=MTid->id;
+				}
+				delete [] arr;
+			}
+			losesize=newlose;
+			break;
+		}
+	}
 
 	return;
 }
@@ -45,6 +233,86 @@ void Problem1::rearrange(Graph &G, Forest &MTidForest) {
 	   Note: Please include "all" active mutlicast trees in MTidForest. */
 
 	/* Write your code here. */
+	int numtree=MTidForest.trees.size();
+	losesize=0;
+	for(auto it:G.E){
+		for(auto &gE:G.E){
+			gE.b=gE.be;
+		}
+	}
+	for(auto nowtree=MTidForest.trees.begin();nowtree!=MTidForest.trees.cend();nowtree++){
+		nowtree->V.clear();
+		nowtree->E.clear();
+		nowtree->ct=0;
+	}
+
+
+	for(auto nowtree=MTidForest.trees.begin();nowtree!=MTidForest.trees.cend();nowtree++){
+		//cout<<inf[nowtree->id].stop<<nowtree->V.size()<<inf[nowtree->id].all.size()<<endl;
+		if(inf[nowtree->id].stop) continue;
+		auto MTid=nowtree;
+		int s=MTid->s;
+		int t=inf[MTid->id].t;
+		auto dis=inf[MTid->id].all;
+		bool* arr=new bool[100001]();
+
+		priority_queue<graphEdge*, vector<graphEdge*>, cmp> pq;
+		int now=s;
+		MTid->V.push_back(now);
+		arr[s]=true;
+
+		for(auto iter:G.E){
+			for(auto &gE:G.E){
+				if(gE.vertex[0]==now&&!arr[gE.vertex[1]]){
+					pq.push(&gE);
+				}
+				if(gE.vertex[1]==now&&!arr[gE.vertex[0]]){
+					pq.push(&gE);
+				}
+			}
+			while(!pq.empty()){
+				//cout<<"{ "<<pq.top()->vertex[0]<<" "<<pq.top()->vertex[1]<<" }";
+				auto gE=pq.top();
+				if(gE->b>=t){
+					//cout<<gE->ce<<" ";
+					if(!(arr[gE->vertex[1]])){
+						gE->b-=t;
+						treeEdge Te;
+						Te.vertex[0]=gE->vertex[0];
+						Te.vertex[1]=gE->vertex[1];
+						arr[gE->vertex[1]]=true;
+						MTid->E.push_back(Te);
+						MTid->ct+=(gE->ce);
+						//cout<<pq.top()->ce<<endl;
+						now=gE->vertex[1];
+						MTid->V.push_back(now);
+						pq.pop();
+						break;
+					}
+					else if(!(arr[gE->vertex[0]])){
+						gE->b-=t;
+						treeEdge Te;
+						Te.vertex[0]=gE->vertex[0];
+						Te.vertex[1]=gE->vertex[1];
+						arr[gE->vertex[0]]=true;
+						MTid->E.push_back(Te);
+						MTid->ct+=(gE->ce);
+						//cout<<pq.top()->ce<<endl;
+						now=gE->vertex[0];
+						MTid->V.push_back(now);
+						pq.pop();
+						break;
+					}
+				}
+				pq.pop();
+			}
+		}
+		if(MTid->V.size()<dis.size()){
+			loseid[losesize++]=MTid->id;
+		}
+		delete [] arr;
+	}
 	
 	return;
 }
+
